e12571c14863bd11eb31be480b9615ee4b0d9314
Tahmid Sajin <64264899+tahmid-saj@users.noreply.github.com>
2023-07-17 23:44:44 -0400

    Create q463.java

---
+++ q463.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 0) {
+                    continue;
+                } else if (grid[i][j] == 1) {
+                    if (j == 0 || grid[i][j - 1] == 0) {
+                        perimeter++;
+                    }
+                    if (i == 0 || grid[i - 1][j] == 0) {
+                        perimeter++;
+                    }
+                    if (j == grid[i].length - 1 || grid[i][j + 1] == 0) {
+                        perimeter++;
+                    }
+                    if (i == grid.length - 1 || grid[i + 1][j] == 0) {
+                        perimeter++;
+                    }
+                }
+            }
+        }
+
+        return perimeter;
+    }
+}
